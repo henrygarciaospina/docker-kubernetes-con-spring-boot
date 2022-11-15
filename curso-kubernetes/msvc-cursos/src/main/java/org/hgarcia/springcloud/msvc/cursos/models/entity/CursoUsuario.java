38bee71be537bb9b4a9390ed792a9d2f184c14af
hegaro <henrygarcia0309@gmail.com>
2022-11-15 16:06:34 -0500

    Se crea JPA entity intermedia CursoUsuario

--- curso-kubernetes/msvc-cursos/src/main/java/org/hgarcia/springcloud/msvc/cursos/models/entity/CursoUsuario.java
+++ curso-kubernetes/msvc-cursos/src/main/java/org/hgarcia/springcloud/msvc/cursos/models/entity/CursoUsuario.java
@@ -2,45 +2,61 @@ package org.hgarcia.springcloud.msvc.cursos.models.entity;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
-import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Table(name  = "cursos")
-public class Curso {
+@Table(name = "cursos_usuarios")
+public class CursoUsuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotEmpty
     private String nombre;
-
-    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
     private List<CursoUsuario> cursoUsuarios;
-    public Curso (){
-        cursoUsuarios = new ArrayList<>();
-    }
+    @Column(name = "usuario_id", unique = true)
+    private Long usuarioId;
+
     public Long getId() {
         return id;
     }
+
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-    public void addCursoUsuario(CursoUsuario cursoUsuario){
-        cursoUsuarios.add(cursoUsuarios);
-    }
-    public void removeCursoUsuario(CursoUsuario cursoUsuario){
-        cursoUsuarios.remove(cursoUsuarios);
-    }
+
     public List<CursoUsuario> getCursoUsuarios() {
         return cursoUsuarios;
     }
+
     public void setCursoUsuarios(List<CursoUsuario> cursoUsuarios) {
         this.cursoUsuarios = cursoUsuarios;
+    }
+
+    public Long getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(Long usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj){
+            return true;
+        }
+        if (!(obj instanceof CursoUsuario)){
+            return false;
+        }
+        CursoUsuario o = (CursoUsuario) obj;
+        return this.usuarioId != null && this.usuarioId.equals(o.usuarioId);
     }
 }
