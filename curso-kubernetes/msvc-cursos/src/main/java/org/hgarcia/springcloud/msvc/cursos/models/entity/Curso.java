5c89f1f91543e1e9bb9fd29fc630822ecd86712a
hegaro <henrygarcia0309@gmail.com>
2022-11-15 16:45:22 -0500

    Añadiendo JoinColumn curso_id como foreignkey y el POJO Usuario

--- curso-kubernetes/msvc-cursos/src/main/java/org/hgarcia/springcloud/msvc/cursos/models/entity/Curso.java
+++ curso-kubernetes/msvc-cursos/src/main/java/org/hgarcia/springcloud/msvc/cursos/models/entity/Curso.java
@@ -1,5 +1,7 @@
 package org.hgarcia.springcloud.msvc.cursos.models.entity;
 
+import org.hgarcia.springcloud.msvc.cursos.models.Usuario;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import java.util.ArrayList;
@@ -13,11 +15,15 @@ public class Curso {
     private Long id;
     @NotEmpty
     private String nombre;
-
     @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "curso_id")
     private List<CursoUsuario> cursoUsuarios;
+
+    @Transient
+    private List<Usuario> usuarios;
     public Curso (){
         cursoUsuarios = new ArrayList<>();
+        usuarios = new ArrayList<>();
     }
     public Long getId() {
         return id;
@@ -32,7 +38,7 @@ public class Curso {
         this.nombre = nombre;
     }
     public void addCursoUsuario(CursoUsuario cursoUsuario){
-        cursoUsuarios.add(cursoUsuarios);
+        cursoUsuarios.add(cursoUsuario);
     }
     public void removeCursoUsuario(CursoUsuario cursoUsuario){
         cursoUsuarios.remove(cursoUsuarios);
@@ -42,5 +48,11 @@ public class Curso {
     }
     public void setCursoUsuarios(List<CursoUsuario> cursoUsuarios) {
         this.cursoUsuarios = cursoUsuarios;
+    }
+    public List<Usuario> getUsuarios() {
+        return usuarios;
+    }
+    public void setUsuarios(List<Usuario> usuarios) {
+        this.usuarios = usuarios;
     }
 }
